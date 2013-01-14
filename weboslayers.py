bd6c8b4d0683a31a78854768a8907bbd053033cd
Patrick Roberts <patrick@silentbluesystems.com>
2013-01-14 13:00:49 -0800

    Fix for mcf setting duplicate/invalid layers for layers with the same priority as meta-oe

--- weboslayers.py
+++ weboslayers.py
@@ -7,8 +7,8 @@ webos_layers = [
 #('meta-oe',           6, 'git://github.com/openembedded/meta-oe.git',        'branch=danny,commit=f026e96', ''  ),
 ('meta-webos',       10, 'git://github.com/openwebos/meta-webos.git',        'branch=preview',              ''  ),
 ('meta-intel',        5, 'git://git.yoctoproject.org/meta-intel.git',        'branch=danny',                ''  ),
-('meta-sugarbay',     6, '',                                                 '',                    'meta-intel/meta-sugarbay'),
-('meta-cedartrail',   6, '',                                                 '',                    'meta-intel/meta-cedartrail'),
+('meta-sugarbay',     7, '',                                                 '',                    'meta-intel/meta-sugarbay'),
+('meta-cedartrail',   7, '',                                                 '',                    'meta-intel/meta-cedartrail'),
 ('meta-rock',        15, 'git://github.com/webOS-ports/meta-rock.git',   'branch=halfhalo/refactor',    ''  ),
 ('meta-slate',       16, '',                                                 '',                    'meta-rock/meta-slate'),
 ]
