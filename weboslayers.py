bd7efff239178748da50bec8a41e4cc4020a6756
Patrick Roberts <patrick@silentbluesystems.com>
2013-01-13 22:01:15 -0800

    Test swapping the layer for a different one

--- weboslayers.py
+++ weboslayers.py
@@ -4,7 +4,7 @@ webos_layers = [
 ('bitbake',          -1, 'git://github.com/openembedded/bitbake.git',        'branch=1.16,commit=34a8d45',  ''  ),
 ('meta',              5, 'git://github.com/openembedded/oe-core.git',        'branch=danny,commit=8e057a5', ''  ),
 ('meta-oe',           6, 'git://github.com/openembedded/meta-oe.git',        'branch=danny,commit=f026e96', ''  ),
-('meta-webos',       10, 'git://github.com/openwebos/meta-webos.git',        'branch=preview',              ''  ),
+('meta-webos',       10, 'git://github.com/webOS-ports/meta-webos.git',        'branch=master-next',              ''  ),
 ('meta-intel',        5, 'git://git.yoctoproject.org/meta-intel.git',        'branch=danny',                ''  ),
 ('meta-sugarbay',     6, '',                                                 '',                    'meta-intel/meta-sugarbay'),
 ('meta-rock',        15, 'git://git.github.com/webOS-ports/meta-rock.git',   'branch=halfhalo/refactor',    ''  ),
