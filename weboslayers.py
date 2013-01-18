a08d9f3096252cff099fc83ff63230c68cd61bed
Patrick Roberts <patrick@silentbluesystems.com>
2013-01-17 16:08:55 -0800

    Move oe-core to danny head to fix hddimg udev bug

--- weboslayers.py
+++ weboslayers.py
@@ -2,7 +2,7 @@ Distribution = "webos"
 
 webos_layers = [
 ('bitbake',          -1, 'git://github.com/openembedded/bitbake.git',        'branch=1.16,commit=34a8d45',  ''  ),
-('meta',              5, 'git://github.com/openembedded/oe-core.git',        'branch=danny,commit=8e057a5', ''  ),
+('meta',              5, 'git://github.com/openembedded/oe-core.git',        'branch=danny', ''  ),
 ('meta-oe',          6, 'git://github.com/openwebos/meta-oe.git',            'branch=preview', ''),
 #('meta-oe',           6, 'git://github.com/openembedded/meta-oe.git',        'branch=danny,commit=f026e96', ''  ),
 ('meta-webos',       10, 'git://github.com/openwebos/meta-webos.git',        'branch=preview',              ''  ),
