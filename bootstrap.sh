26324adc473fa6face0f7951e20fe47c0d38cd97
Patrick Roberts <patrick@silentbluesystems.com>
2013-02-12 20:14:03 -0800

    Add bootstrap script to make building for rock easier to resetup

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#/bin/sh
+MACH=$1
+if [ -z "$1" ]
+then
+MACH="rock"
+fi
+echo 'git clone git://github.com/halfhalo/build-webos.git'
+echo `git clone git://github.com/halfhalo/build-webos.git`
+echo 'cd build-webos'
+echo 'git checkout remote-layers'
+echo `cd build-webos && git checkout remote-layers`
+echo "./mcf -p0 -b0 -r https://raw.github.com/halfhalo/rock-layers/master/weboslayers.py $MACH"
+echo `cd build-webos && ./mcf -p0 -b0 -r https://raw.github.com/halfhalo/rock-layers/master/weboslayers.py '$MACH'`
