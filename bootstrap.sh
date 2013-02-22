b049e6e6486113d397925b2b56c0b31f46d3b39d
Patrick Roberts <patrick@silentbluesystems.com>
2013-02-21 19:22:13 -0800

    Tweak bootstrap.sh for new test layer

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,6 +10,6 @@ echo 'cd build-webos'
 echo 'git checkout remote-layers'
 echo `cd build-webos && git checkout remote-layers`
 echo "./mcf -p0 -b0 -r https://raw.github.com/halfhalo/rock-layers/master/weboslayers.py $MACH"
-echo `cd build-webos && ./mcf -p0 -b0 -r https://raw.github.com/halfhalo/rock-layers/master/weboslayers.py $MACH`
+echo `cd build-webos && ./mcf -p0 -b0 -r https://github.com/halfhalo/rock-layers/blob/envy/oe-core-uefi/weboslayers.py $MACH`
 cd build-webos
 echo "To build, just type make webos-image"
